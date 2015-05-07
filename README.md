90a6d44f9e44d8188900a1f7f327892e8ffb24bf
otarim <otarim.com@gmail.com>
2015-05-07 11:48:59 +0800

    1.0

--- README.md
+++ README.md
@@ -1,2 +1,33 @@
 # hash.route
-frontEnd route using hash
+前端路由，使用 hash
+
+##用法
+
+**R(config)**
+
+创建一个路由实例
+
+	config:
+		base: 子路由的路径，默认 / 
+		separator: 识别路由的分隔符，默认为 !，也就是 #! 后面的路径为路由
+		
+**when(condition,callback)**
+
+添加路由表
+
+	condition: 路由匹配，支持正则
+	callback: 路由匹配后的回调，正则匹配的分组结果会左右参数传入 callback 中
+		req: callback 的第一个参数，保存请求数据
+			req.query 请求参数
+			
+**R.run()**
+
+运行路由
+
+##例子
+
+	/test/index.html
+
+
+
+
---
+++ dist/index.js
@@ -0,0 +1,101 @@
+(function(global,factory){
+	if(typeof define === 'function' && define.cmd){
+		return define(factory)
+	}else{
+		global.R = factory()
+	}
+})(this,function(){
+	var isReg = function(input){
+		return Object.prototype.toString.call(input) === "[object RegExp]"
+	}
+	var isEqualPath = function(hostname){
+		var hash = location.hash
+		return hash.slice(0,hostname.length) === hostname
+	}
+	var getQuery = function(url){
+		var ret = {}
+		if(url.indexOf('?') !== -1){
+			var query = url.slice(url.lastIndexOf('?') + 1)
+			if(query){
+				query = query.split('&')
+				query.forEach(function(q){
+					q = q.split('=')
+					if(ret[q[0]]){
+						ret[q[0]] = [].concat(ret[q[0]])
+						ret[q[0]].push(q[1])
+					}else{
+						ret[q[0]] = q[1]
+					}
+				})
+			}
+		}
+		return ret
+	}
+	var R = function(conf){
+		var self = this
+		if(!(self instanceof R)){
+			self = Object.create(R.prototype)
+		}
+		self.separator = conf.separator ||  '!'
+		self.base = conf.base || '/'
+		self.hostname = '#' + self.separator + self.base
+		self.urlFrom = self.hostname.length
+		return self
+	}
+
+	R.prototype = {
+		constructor: R,
+		when: function(condition,cb){
+			R.queue.push(this.addRoute.bind(this,condition,cb))
+			return this
+		},
+		getPathname: function(){
+			var ret = location.hash.slice(this.urlFrom)
+			if(ret.indexOf('?') !== -1){
+				ret = ret.slice(0,ret.indexOf('?'))
+			}
+			return ret
+		},
+		addRoute: function(condition,cb){
+			if(isEqualPath(this.hostname)){
+				var pathname = this.getPathname()
+				if(isReg(condition)){
+					var match = condition.exec(pathname)
+					if(match){
+						cb.apply(this,[{
+							query: getQuery(location.hash)
+						}].concat(match.slice(1)))
+					}
+				}else{
+					if(condition === pathname){
+						cb.call(this,{
+							query: getQuery(location.hash)
+						})
+					}
+				}
+			}
+			R.currentHash = location.hash
+			return this
+		}
+	}
+	// static
+	R.bindStatus = false
+	R.currentHash = location.hash
+	R.queue = []
+	R.checkQueue = function(){
+		R.queue.forEach(function(q){
+			q()
+		})
+	}
+	R.run = function(){
+		if(!R.bindStatus){
+			R.bindStatus = true
+			window.addEventListener('hashchange',function(){
+				if(R.currentHash === location.hash) return
+				R.checkQueue()
+			},false)
+		}
+		R.checkQueue()
+	}
+	return R
+})
